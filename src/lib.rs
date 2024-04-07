2a2e93d0e6b27c6da398492578b6772c80e343da
eddie <edward.pokhylko@gmail.com>
2024-04-07 22:20:39 +0200

    Barebones lsb

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,34 @@
 mod error;
 use error::CliError;
 
-trait Steganography {
-    fn embed(&self, image_data: &mut Vec<u8>, secret_data: &[u8]) -> Result<(), CliError>;
-    fn extract(&self, image_data: &[u8]) -> Result<Vec<u8>, CliError>; 
+use bitvec::prelude::*;
+
+pub trait Steganography {
+    fn embed(&self, image_data: &mut BitVec<u8>, secret_data: &[u8]) -> Result<(), CliError>;
+    fn extract(&self, image_data: &BitVec<u8>, secret_length: usize) -> Result<Vec<u8>, CliError>; 
 }
 
-struct LeastBit {}
+pub struct LeastBit {}
+impl Steganography for LeastBit {
+    fn embed(&self, image_data: &mut BitVec<u8>, secret_data: &[u8]) -> Result<(), CliError> {
+        let secret_bits: BitVec<u8> = BitVec::from_slice(secret_data);
+        // testing the ability to store!
+        if image_data.len() / 8 < secret_bits.len() {
+            return Err(CliError::Image("Provided image is too small to store your data.".to_string()));
+        }
+        
+        for (i, bit) in secret_bits.iter().enumerate() {
+            let bit_i = i * 8;
+            image_data.set(bit_i, *bit);
+        }
+        Ok(())
+    }
+    fn extract(&self, image_data: &BitVec<u8>, secret_length: usize) -> Result<Vec<u8>, CliError> {
+        let mut extracted_bits: BitVec<u8> = BitVec::with_capacity(secret_length*8);
+        for i in 0..secret_length*8 {
+            let bit_i = i * 8;
+            extracted_bits.push(image_data[bit_i]);
+        }
+        Ok(extracted_bits.into_vec())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,71 @@
+use pixeldust_wip::LeastBit;
+
 fn main() -> Result<(), Box<dyn std::error::Error>>
 { unimplemented!() }
+
+#[cfg(test)]
+mod spec {
+    use anyhow::Result;
+    use assert_cmd::Command;
+    use assert_fs::prelude::*;
+    use bitvec::vec::BitVec;
+    use image::DynamicImage;
+    
+    use image::{codecs::jpeg::JpegEncoder, RgbImage};
+    use pixeldust_wip::{LeastBit, Steganography};
+    use std::io::Cursor;
+
+    fn create_black_jpeg(width: u32, height: u32) -> Vec<u8> {
+        //creating an initial buffer
+        let mut img = RgbImage::new(width, height);
+
+        // filling our black image
+        for y in 0..height {
+          for x in 0..width {
+            img.put_pixel(x, y, image::Rgb([0, 0, 0]));
+          }
+        }
+        
+        let img = DynamicImage::ImageRgb8(img);
+
+        let mut writer = Cursor::new(Vec::new());
+
+        // encoding as jpeg with a high quality...
+        let mut encoder = JpegEncoder::new_with_quality(&mut writer, 95);
+        encoder.encode(
+          img.as_bytes(),
+          width,
+          height,
+          image::ColorType::Rgb8).expect("Failed to encode.");
+
+        writer.into_inner()
+    }
+
+    #[test]
+    fn embed_lb() -> Result<()> {
+        // take an image
+        // edit the image
+        // it's not the same
+        
+        let steg = LeastBit {};
+
+        let mut experiment: BitVec<u8> = BitVec::from_vec(create_black_jpeg(25, 25));
+        let before = experiment.clone();
+
+        steg.embed(&mut experiment, b"secret!")?;
+        
+        assert_ne!(before, experiment);
+        Ok(())
+    }
+    #[test]
+    fn extract_lb() -> Result<()> {
+        let steg = LeastBit {};
+
+        let mut experiment: BitVec<u8> = BitVec::from_vec(create_black_jpeg(50, 50));
+        steg.embed(&mut experiment, b"secret!")?;
+        let secret = steg.extract(&experiment, 7)?;
+        
+        assert_eq!(String::from_utf8(secret).unwrap(), "secret!");
+        Ok(())
+    }
+}
